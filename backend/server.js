7481d0db0b6b601caacc4ddae8cbbdcc040e12e8
Darshan <darshanpatel2923@gmail.com>
2023-08-26 19:35:28 +0530

    Added route from backend which will make api call

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes); //Created api endpoints to connect frontend from our backend
-app.use('/api/v1/post', dalleRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get("/", async (req, res) => {
     res.send("Serber is running");
