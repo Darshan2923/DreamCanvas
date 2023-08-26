2a4879502f0afe04cbd405bb8c86aaed4f420042
Darshan <darshanpatel2923@gmail.com>
2023-08-26 19:26:51 +0530

    Solved a bug as Configuration is decrepated...

--- backend/routes/dalleRoutes.js
+++ backend/routes/dalleRoutes.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import { OpenAI } from 'openai';
 
 import Post from '../mongodb/models/post.js';
 
 dotenv.config();
 
 const router = express.Router();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+router.route('/').get((req, res) => {
+    res.send("Hello from DALL_E");
+});
+
+export default router;
