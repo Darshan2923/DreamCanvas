3174d97ebb95f9463f35ff04cc5939da543d9085
Darshan <darshanpatel2923@gmail.com>
2023-08-26 18:54:10 +0530

    Baseline for all folders required for our project

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,17 @@ import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
 
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
+
 dotenv.config(); //This allows us to pull variables from dotenv file
 const app = express();
+
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes); //Created api endpoints to connect frontend from our backend
+app.use('/api/v1/post', dalleRoutes);
 
 app.get("/", async (req, res) => {
     res.send("Serber is running");
