fdfa8d396c0e6ff4665fd0dca94c3618e1ea7e3c
Darshan <darshanpatel2923@gmail.com>
2023-08-27 19:39:19 +0530

    Getting the data posted in createPost page at home

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Loader, Card, Formfield } from '../components'
 
@@ -16,6 +16,28 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSetText] = useState('')
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch('http://localhost:5100/api/v1/post', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+
+                })
+                if (response.ok) {
+                    const result = await response.json();
+
+                    setAllPosts(result.data.reverse());
+                }
+            } catch (err) {
+                alert(err);
+            }
+        }
+    }, []);
 
     return (
         <section className='max-w-7xl mx-auto'>
@@ -52,7 +74,7 @@ const Home = () => {
                                 />
                             ) : (
                                 <RenderCards
-                                    data={[]}
+                                    data={allPosts}
                                     title="No Posts Found"
                                 />
                             )}
